c5474c415e80ba6f2c28ee3c3de45223c340fa32
abhayjohri23 <abhay.johri0@gmail.com>
2023-11-05 18:11:32 +0530

    Added frontcover and some functionality

--- Scripts/bubbleLogic.js
+++ Scripts/bubbleLogic.js
@@ -1,9 +1,21 @@
-let initTime = 60;
+let initTime = 120;
 let initScore = Number(document.querySelector('#scoreVal').innerHTML);
 let levelOfDiff = 10;
 let target = parseInt(document.querySelector('#target').innerHTML);
+let negatives = 0;
+
+String.prototype.equals = function(text){
+    const myText = this.toString;
+    if(myText.length != text.length)            return false;
+    for(let i=0;i<myText.length;++i){
+        if(myText.charAt(i) != text.charAt(i))  return false;
+    }
+
+    return true;
+}
 
 function makeBubbles(){
+    getNewTarget();
     let clutter = ``;
     for(let i=1;i<=160;++i){
         clutter += `<div class="bubble">${Math.floor(Math.random()*10)}</div>`;
@@ -20,7 +32,10 @@ function callTimer(){
             timerPlace.innerHTML = initTime;
         }
         else{
-            document.querySelector('#bottomPart').innerHTML = '<h1 class="endOfGame">GAME OVER</h1>';
+            document.querySelector('#bottomPart').innerHTML =`<div class="gameOptions">
+            <h1 class="endOfGame">GAME OVER</h1>
+            <button class="again" onclick="">Play Again</button>
+            </div>`;
             clearInterval(timerID);
         }
     },1000);
@@ -33,14 +48,11 @@ function getNewTarget(){
 }
 
 function increaseScore(){
-    initScore += 10;
+    initScore += (10 - negatives);
     document.querySelector('#scoreVal').innerHTML = initScore;
     console.log(`newScore: ${initScore}`);
 }
 
-
-makeBubbles();
-callTimer();
 
 /*Event Bubbling used. Event triggered on pressing the bubble on the screen triggers the event listener of the parent 
 if the venet listener for the component is not available.
@@ -55,4 +67,26 @@ immedParent.addEventListener('click',function(event){
         }
     }
 });
+
+function startGame(levelOfDifficulty){
+    if(levelOfDifficulty.equals('Hard')){
+        levelOfDiff = 20;
+        initTime = 60;
+        negatives = -5;
+    }
+    else if(levelOfDifficulty.equals('Medium')){        
+        levelOfDiff = 15;
+        initTime = 90;
+    }
+    else{
+        levelOfDiff = 10;
+        initTime = 120;
+    }                                    
+
+    document.querySelector('#startPanel').style.visibility = 'hidden';
+    document.querySelector('#gamePanel').style.visibility = 'visible';
+
+    makeBubbles();
+    callTimer();
+}
 
